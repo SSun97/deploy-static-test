7cd5f1ed3ccad5d1a44106a67fa340a2513fac1e
Simon Sun <sunmingyu.97@gmail.com>
2021-06-11 20:34:44 +0000

    first push

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Fabric function"""
+
+from fabric.api import local
+from datetime import date
+
+def do_pack():
+    """ pack up our web_static"""
+
+    try:
+        now = datetime.now()
+        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        tarArchivePath = "versions/" + tarArchiveName
+    
+        local("mkdir -p versions")
+        local("tar -czvf" + tarArchivePath + " web_static")
+        
+        return tarArchivePath
+    except:
+        return None
